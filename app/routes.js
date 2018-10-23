e83b0222cfe4f0c5037df0ae501760eac952aacf
David Rendell <david.rendell@softwire.com>
2018-10-23 13:41:59 +0100

    Fill in page asking for CPNP reference number

--- app/routes.js
+++ app/routes.js
@@ -18,4 +18,20 @@ router.post('/cosmetics/product-sale-date-check-answer', function (req, res) {
     }
   })
 
+// Branching on whether user has CPNP product reference number
+router.post('/cosmetics/cpnp-reference-submit', function (req, res) {
+  // Get the answer from session data
+  // The name between the quotes is the same as the 'name' attribute on the input elements
+  // However in JavaScript we can't use hyphens in variable names
+
+  let hasCpnpReferenceNumber = req.session.data['cpnp-reference']
+  let cpnpProductReference = req.session.data['cpnp-product-reference']
+
+  if (hasCpnpReferenceNumber === 'false') {
+    res.redirect('/cosmetics/no-product-found')
+  } else {
+    res.redirect('/cosmetics/cpnp-product')
+  }
+})
+
 module.exports = router
