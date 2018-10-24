8ffba1eeaee932ce2b1b498d3a5b7179e9359bb6
David Rendell <david.rendell@softwire.com>
2018-10-24 11:05:24 +0100

    Add settings page, allowing users to set whether the scraped RP requires a UK RP or not

--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,23 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+// Update prototype settings
+router.post('/settings-update', function (req, res) {
+  // Get the answer from session data
+  // The name between the quotes is the same as the 'name' attribute on the input elements
+  // However in JavaScript we can't use hyphens in variable names
+
+  let requireNewRp = req.session.data['require-new-responsible-person']
+
+  if  (requireNewRp === 'true') {
+    res.cookie('requireNewRp', true)
+  } else {
+    res.cookie('requireNewRp', false)
+  }
+
+  res.redirect('/')
+})
+
 // Branching on whether product was on sale pre brexit
 router.post('/cosmetics/product-sale-date-check-answer', function (req, res) {
     // Get the answer from session data
@@ -45,7 +62,7 @@ router.post('/cosmetics/check-cpnp-product-correct', function (req, res) {
   if (productDataIsCorrect === 'false') {
     res.redirect('/cosmetics/search-again')
   } else {
-    res.redirect('/cosmetics/check-product-details')
+    res.redirect('/cosmetics/check-responsible-person')
   }
 })
 
@@ -61,6 +78,16 @@ router.post('/cosmetics/product-regsistered-on-cpnp-check', function (req, res) 
     res.redirect('/cosmetics/cpnp-advice')
   } else {
     res.redirect('/cosmetics/cpnp-export-upload')
+  }
+})
+
+// Either send to product details check, or to section to add new RP depending on 
+// value of settings cookie.
+router.get('/cosmetics/check-responsible-person', function(req, res) {
+  if (req.cookies['requireNewRp'] === 'true') {
+    res.redirect('/cosmetics/responsible-person-name')
+  } else {
+    res.redirect('/cosmetics/check-product-details')
   }
 })
 
