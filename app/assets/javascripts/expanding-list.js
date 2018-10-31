f8a7925822929537b6938fc2d187140da09fc903
David Rendell <david.rendell@softwire.com>
2018-10-31 16:50:31 +0000

    Fix issue where adding new element to expanding list duplicated existing input

--- app/assets/javascripts/expanding-list.js
+++ app/assets/javascripts/expanding-list.js
@@ -1,7 +1,21 @@
 addAnother = function () {
     var list = document.getElementById('expanding-list')
-    var listEntry = list.lastElementChild
+    var listEntry = list.firstElementChild
     var newEntry = listEntry.cloneNode(true)
+
+    // Update ID of inputs, wipe any values they have
+    var inputs = newEntry.getElementsByTagName('input')
+    console.log(inputs)
+    var elementNumber = list.childElementCount + 1
+    for (var i = 0; i < inputs.length; i++) {
+        console.log(inputs[i])
+        inputs[i].id = inputs[i].id + '-' + elementNumber
+        inputs[i].name = inputs[i].name + '-' + elementNumber
+        inputs[i].value = ''
+        inputs[i].checked = false
+    }
+
+
     list.appendChild(newEntry)
 }
     
