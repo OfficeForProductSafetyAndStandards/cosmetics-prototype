24b1621c0faf535a1f7f4b72140a8730f2db8941
David Rendell <david.rendell@softwire.com>
2018-11-12 13:55:10 +0000

    Content updates

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Notify cosmetics - Prototype',
+  serviceName: 'Notify cosmetics',
 
   // Default port that prototype runs on
   port: '3000',
