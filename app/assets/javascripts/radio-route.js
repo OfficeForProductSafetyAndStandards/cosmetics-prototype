fb70fa039446b5e6f5f499b329f1242451f76efa
David Rendell <david.rendell@softwire.com>
2018-11-16 11:06:01 +0000

    Avoid error messages if users don't pick a radio option

--- app/assets/javascripts/radio-route.js
+++ app/assets/javascripts/radio-route.js
@@ -1,22 +1,28 @@
 
 /*
-* Method attached to the submit event of forms in the radio-route macro. This function prevents the form submission, and instead sends it as an AJAX request, and then instead sends the user to the page for the route they've picked
+* Method attached to the submit event of forms in the radio-route macro. This 
+* function prevents the form submission, and instead sends it as an AJAX 
+* request, and then instead sends the user to the page for the route they've 
+* picked.
 */
 routeToRadioChoice = function(event) {
     event.preventDefault()
 
-    // Get the URL of the checked radio
-    var url = $("#route-form input:checked").data('route')
+    // If there is a checked radio button, route to that URL. Otherwise do nothing.
+    if ($("#route-form input:checked").length) {
+        // Get the URL of the checked radio
+        var url = $("#route-form input:checked").data('route')
 
-    // Submit the form as AJAX, then redirect to the URL when done.
-    // The form route doesn't even have to exist, this just makes 
-    // sure the data is saved in the session.
-    $.ajax({
-        url: '/radio-route',
-        type: 'post',
-        data: $('#route-form').serialize(),
-        success: function() {
-            window.location.href = url
-        }
-    })
+        // Submit the form as AJAX, then redirect to the URL when done.
+        // The form route doesn't even have to exist, this just makes 
+        // sure the data is saved in the session.
+        $.ajax({
+            url: '/radio-route',
+            type: 'post',
+            data: $('#route-form').serialize(),
+            success: function() {
+                window.location.href = url
+            }
+        })
+    }
 }
