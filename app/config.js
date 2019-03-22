604706b884f7b65039698b7982a59922fafe3102
Ralph <ralph.hawkins@digital.cabinet-office.gov.uk>
2019-03-22 15:02:11 +0000

    update content

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Register cosmetics products',
+  serviceName: 'Submit cosmetic product notifications',
 
   // Default port that prototype runs on
   port: '3000',
